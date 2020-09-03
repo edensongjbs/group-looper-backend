a7b42c43c03e499533d708eaa7c249ce67770006
edensongjbs <edensongjbs@gmail.com>
2020-09-03 16:22:38 -0400

    delete layer working

--- app/controllers/layers_controller.rb
+++ app/controllers/layers_controller.rb
@@ -3,9 +3,17 @@ class LayersController < ApplicationController
         layer = Layer.create!(layer_params)
         composition = layer.composition
         # this is where we'll do the Action Cable stuff
-        CompositionsChannel.broadcast_to(composition, layer.serialize)
+        CompositionsChannel.broadcast_to(composition, {action:'ADD_LAYER', layer:layer.serialize})
         # byebug
         render json: {oldId:layer.temporary_id, newId: layer.id}
+    end
+
+    def destroy
+        layer = Layer.find(params[:id])
+        composition = layer.composition
+        layer.destroy
+        CompositionsChannel.broadcast_to(composition, {action:'DELETE_LAYER', layerId:params[:id].to_i})
+        render json: {message: "Layer successfully deleted"}
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create, :delete, :show, :update]
-  resources :compositions, only: [:create, :show, :delete, :update]
-  resources :layers, only: [:create, :show, :delete, :update]
+  resources :users, only: [:create, :destroy, :show, :update]
+  resources :compositions, only: [:create, :show, :destroy, :update]
+  resources :layers, only: [:create, :show, :destroy, :update]
   mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
