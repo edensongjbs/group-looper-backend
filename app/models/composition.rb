0b28fcf0d86698e2063e5e939a0316a6bfdecfae
edensongjbs <edensongjbs@gmail.com>
2020-09-07 20:22:07 -0400

    update composition model

--- app/models/composition.rb
+++ app/models/composition.rb
@@ -1,6 +1,8 @@
 class Composition < ApplicationRecord
     belongs_to :creator, class_name: 'User'
     has_many :layers
+    has_many :user_compositions
+    has_many :users, through: :user_compositions
 
     def serialize_layers(user)
         self.layers.to_a.map{ |l| l.serialize(user) }
